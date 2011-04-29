4779f50eae99bf8421e9a864a6491568789f7c1c
Jen Smith <jennifer@home>
2011-04-29 12:11:43 +0100

    Added another article

--- app.rb
+++ app.rb
@@ -4,7 +4,8 @@ require 'sinatra'
 get '/' do
 	redirects_by_param = {
 		nil => "http://www.jennifersmith.co.uk",
-		"120"=> "http://www.jennifersmith.co.uk/blog/2009/05/29/spark-codec-for-openrasta/index.html"
+		"120"=> "http://www.jennifersmith.co.uk/blog/2009/05/29/spark-codec-for-openrasta/index.html",
+		"265"=> "http://www.jennifersmith.co.uk/blog/2011/03/27/qcon-2011-friday-session-node-js-asynchronous-io-for-fun-and-profit/index.html"
 	}
   p_param = params[:p]
 	pass unless redirects_by_param.has_key? p_param
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -7,6 +7,11 @@ describe "App" do
 		last_response.should be_redirect
 		last_response.headers["Location"].should == 'http://www.jennifersmith.co.uk/blog/2009/05/29/spark-codec-for-openrasta/index.html'
   end
+	it "should respond to /?p=120 to correct article" do
+    get '/?p=265'
+		last_response.should be_redirect
+		last_response.headers["Location"].should == 'http://www.jennifersmith.co.uk/blog/2011/03/27/qcon-2011-friday-session-node-js-asynchronous-io-for-fun-and-profit/index.html'
+  end
 	it "should redirect requests for / directly to jennifersmith.co.uk" do
 	  get "/"
 		last_response.should be_redirect
