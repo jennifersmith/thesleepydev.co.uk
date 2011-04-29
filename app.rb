05ac6851ccc56da78eae938d20a0dcf75bc08c07
Jen Smith <jennifer@home>
2011-04-29 12:05:24 +0100

    Fixed redirects to be a bit more extendible and redrected /

--- app.rb
+++ app.rb
@@ -2,9 +2,13 @@ require 'rubygems'
 require 'sinatra' 
 
 get '/' do
-	params_to_match = ["120"]
-	pass unless params_to_match.include? params[:p]
-	redirect "http://www.jennifersmith.co.uk/blog/2009/05/29/spark-codec-for-openrasta/index.html"
+	redirects_by_param = {
+		nil => "http://www.jennifersmith.co.uk",
+		"120"=> "http://www.jennifersmith.co.uk/blog/2009/05/29/spark-codec-for-openrasta/index.html"
+	}
+  p_param = params[:p]
+	pass unless redirects_by_param.has_key? p_param
+	redirect redirects_by_param[p_param]
 end
 
 get '/*' do 
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -7,10 +7,13 @@ describe "App" do
 		last_response.should be_redirect
 		last_response.headers["Location"].should == 'http://www.jennifersmith.co.uk/blog/2009/05/29/spark-codec-for-openrasta/index.html'
   end
+	it "should redirect requests for / directly to jennifersmith.co.uk" do
+	  get "/"
+		last_response.should be_redirect
+		last_response.headers["Location"].should == 'http://www.jennifersmith.co.uk'
+	end
 	it "should send all other requests to the catchall view" do
 		get "/foo"
-		response.should have_selector "#catchallredirect"
-	  get "/"
 		response.should have_selector "#catchallredirect"
 		get "/?p=101010"
 		response.should have_selector "#catchallredirect"
